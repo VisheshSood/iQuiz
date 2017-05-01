30141c6bc6a156f4d2e1c60b2d695e07566301d2
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-05-01 16:45:31 -0700

    complete

--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+  
+  let topicHeadings = ["Mathematics", "Marvel Super Heroes", "Science"]
+  let topicDescription = ["Simple Arithmetic","Comic Questions","Things That Dont Make Sense"]
+  let topicIcons = ["Math.png","Marvel.png","Science.png"]
+  
+  
+  @IBAction func settingButton(_ sender: UIBarButtonItem) {
+    let view = UIAlertController(title: "Settings Pressed", message: "OK", preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    view.addAction(defaultAction)
+    self.present(view, animated: true, completion: nil)
+  }
+  
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return topicHeadings.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let row = UITableViewCell(style: .subtitle, reuseIdentifier: "Row")
+    row.imageView?.image = UIImage(named: topicIcons[indexPath.row])
+    row.textLabel?.text = topicHeadings[indexPath.row]
+    row.detailTextLabel?.text = topicDescription[indexPath.row]
+    row.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+    
+    return row
+  }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    tableView.tableFooterView = UIView()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  
 }
 
